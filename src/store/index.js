e42ebdfe692115a88460b864474fed876874dc35
Reversat-Vincent <vincent.reversat@ovh.fr>
2021-12-06 16:04:38 +0100

    add conversation to store

--- src/store/index.js
+++ src/store/index.js
@@ -69,7 +69,7 @@ export default new Vuex.Store({
         });
     },
     conversation(state, getters) {
-      console.log(getters);
+      return getters.conversations.find(el => el.id == state.currentConversationId);
     }
   },
   mutations: {
